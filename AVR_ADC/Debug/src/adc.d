3af9a09403692428efacb7ec49b209d10782caf3
idachi rufusy <ruffiam@yahoo.com>
2018-05-09 15:05:16 +0300

    Add files via upload

---
+++ AVR_ADC/Debug/src/adc.d
@@ -0,0 +1,5 @@
+src/adc.o: ../src/adc.c ../src/stdutils.h ../src/adc.h
+
+../src/stdutils.h:
+
+../src/adc.h:
---
+++ AVR_ADC/Debug/src/main.d
@@ -0,0 +1,5 @@
+src/main.o: ../src/main.c ../src/stdutils.h ../src/adc.h
+
+../src/stdutils.h:
+
+../src/adc.h:
